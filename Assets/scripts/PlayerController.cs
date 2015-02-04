84ac51dace8326e46229425c6ae03470b7498ba3
Dylan Davidson <ddavidson621@gmail.com>
2015-02-04 16:01:29 -0500

    Adds controls for Player.

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float movementSpeed = 2f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.Rotate (0f, 0f, -2.5f);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Rotate (0f, 0f, 2.5f);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += transform.up * Time.deltaTime * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= transform.up * Time.deltaTime * movementSpeed;
+		}
+	}
+}
