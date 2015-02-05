27bb61bae5d7b2a6f9b8eb464af8d7fa68608f28
Dylan Davidson <ddavidson621@gmail.com>
2015-02-05 01:00:43 -0500

    Adds ability to remove agents by right clicking.

--- Assets/scripts/AgentController.cs
+++ Assets/scripts/AgentController.cs
@@ -11,5 +11,14 @@ public class AgentController : MonoBehaviour {
 			mousePos.z = 0;
 			Instantiate(agent, mousePos, Quaternion.identity);
 		}
+		if (Input.GetMouseButtonDown (1)) {
+			Vector3 mousePos = Input.mousePosition;
+			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+			Vector2 v2 = new Vector2(mousePos.x, mousePos.y);
+			Collider2D collidedObj = Physics2D.OverlapPoint(v2);
+			if(collidedObj != null && collidedObj.gameObject.CompareTag("agent")) {
+				Destroy(collidedObj.gameObject);
+			}
+		}
 	}
 }
