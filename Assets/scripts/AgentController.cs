5a1d7071841eaa0a4dac00e8e75f3af6db4e14e1
Dylan Davidson <ddavidson621@gmail.com>
2015-02-05 00:37:17 -0500

    Adds agent prefab, and ability to add agents by clicking.

---
+++ Assets/scripts/AgentController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentController : MonoBehaviour {
+	public Transform agent;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 mousePos = Input.mousePosition;
+			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+			mousePos.z = 0;
+			Instantiate(agent, mousePos, Quaternion.identity);
+		}
+	}
+}
