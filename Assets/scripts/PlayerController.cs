26071b7328850b438a551286dc2892c4f63ae58a
Daniel <drealnn@gmail.com>
2015-02-05 22:40:36 -0500

    Added sensor as child of Player object with a trigger collider. Made changes to Player Controller script so that the number of agents and walls sensed by trigger are sent to console. (unselect collapse on console for proper output)

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,21 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 	public float movementSpeed = 2f;
+	private int numWalls = 0;
+	private int numAgents = 0;
 
-	void Update () {
+	void Update () 
+	{
+
 		if (Input.GetKey (KeyCode.RightArrow)) {
 				transform.Rotate (0f, 0f, -2.5f);
+				
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 				transform.Rotate (0f, 0f, 2.5f);
+
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.position += transform.up * Time.deltaTime * movementSpeed;
+
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.position -= transform.up * Time.deltaTime * movementSpeed;
+
 		}
+
+
+		//Physics2D.CircleCastAll(transform.position, 2, 
+	}
+
+	void OnTriggerEnter2D( Collider2D col )
+	{
+		if (col.gameObject.tag == "agent") 
+		{
+			numAgents++;
+
+		}
+
+		if (col.gameObject.tag == "wall") 
+		{
+			numWalls++;
+		}
+
+		print ("numAgents: " + numAgents + "\n");
+		print ("numWalls: " + numWalls + "\n");
+
+	}
+
+	void OnTriggerExit2D( Collider2D col )
+	{
+		if (col.gameObject.tag == "agent") 
+		{
+			numAgents--;
+
+		}
+
+		if (col.gameObject.tag == "wall") 
+		{
+			numWalls--;
+		}
+
+		print ("numAgents: " + numAgents + "\n");
+		print ("numWalls: " + numWalls + "\n");
 	}
 }
