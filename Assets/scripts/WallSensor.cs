7a4caec1fe329d289b87d201e229b1964478406c
TrevorRoman <troman360@gmail.com>
2015-02-11 16:42:55 -0500

    Right sensor partial functionality
    It's accurately calculating distance and scaling but I still haven't
    figured out the angle issue

--- Assets/scripts/WallSensor.cs
+++ Assets/scripts/WallSensor.cs
@@ -7,47 +7,54 @@ public class WallSensor : MonoBehaviour {
 	private float distanceForward;
 	private float distanceRight;
 	private float distanceLeft;
-	private Vector2 rightAngle;
+	private Vector2 rightVector;
+	private RaycastHit2D forwardSensor;
+	private RaycastHit2D rightSensor;
+
 	//private Quaternion leftAngle = Quaternion.AngleAxis(-15, new Vector3(0, 1, 0));
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
-		//rightAngle = new Vector2 ();
-		//leftAngle = new Vector2 ();
-		
-		
-		RaycastHit2D forwardSensor = Physics2D.Raycast (transform.position,
-		                                                transform.up, 
-		                                                range, 
-		                                                1 << LayerMask.NameToLayer("Walls"));/*
-		RaycastHit2D rightSensor = Physics2D.Raycast (rightTransform.position,
-		                                              rightTransform.up,
-		                                              range, 
-		                                              1 << LayerMask.NameToLayer("Walls"));/*
+
+		rightVector = new Vector2(transform.up.y * Mathf.Tan(45), transform.up.y).normalized;
+
+		forwardSensor = Physics2D.Raycast (transform.position,
+		                                   transform.up, 
+		                                   range, 
+		                                   1 << LayerMask.NameToLayer("Walls"));
+
+		rightSensor = Physics2D.Raycast (transform.position,
+		                                 rightVector,
+		                                 range, 
+		                                 1 << LayerMask.NameToLayer("Walls"));
+		/*
 		RaycastHit2D leftSensor = Physics2D.Raycast (transform.position,
 		                                             leftAngle,
 		                                             range, 
 		                                             1 << LayerMask.NameToLayer("Walls"));*/
-		if(forwardSensor.collider != null)
-		{
-			distanceForward = Mathf.Abs(forwardSensor.point.y - transform.position.y);
-			Debug.Log("Forward Distance: " + distanceForward);
-			Debug.DrawRay (transform.position, transform.up, Color.red);
+		if (forwardSensor.collider != null) {
+			distanceForward = Vector2.Distance(forwardSensor.point, transform.position);
+			Debug.Log ("Forward Distance: " + distanceForward);
+			Debug.DrawRay (transform.position, transform.up * distanceForward, Color.red);
+		} else {
+			Debug.DrawRay (transform.position, transform.up * 1f, Color.red);
 		}
-		/*
+
 		if(rightSensor.collider != null)
 		{
-			distanceRight = Mathf.Abs(rightSensor.point.y - transform.position.y);
+			distanceRight = Vector2.Distance(rightSensor.point, transform.position);
 			Debug.Log("Right Distance: " + distanceRight);
-			Debug.DrawRay (rightTransform.up, rightTransform.up, Color.green);
+			Debug.DrawRay (transform.position, rightVector * distanceRight, Color.green);
+		} else {
+			Debug.DrawRay (transform.position, rightVector * distanceRight, Color.green);
 		}
-/*
-		if(leftSensor.collider != null)
+
+		/*if(leftSensor.collider != null)
 		{
 			distanceLeft = Mathf.Abs(leftSensor.point.y - transform.position.y);
 			Debug.Log("Left Distance: " + distanceLeft);
 			Debug.DrawRay (transform.position, leftAngle, Color.yellow);
 		}*/
+
 	}
 }
