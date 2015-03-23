6d38085e07e0ad4283cae34e485147ad3fd67883
Dylan Davidson <ddavidson621@gmail.com>
2015-03-23 15:42:33 -0400

    Remove bloat.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,12 +7,10 @@ public class PlayerController : MonoBehaviour {
 	public float movementSpeed = 2f;
 	public float autoMoveSpeed = 5f;
 	public List<GameObject> adjacentAgents = new List<GameObject>();
-	public Animator animate;
 	public Vector3 target;
 
 	void Start() {
 		GameObject sprite = transform.FindChild("PlayerSprite").gameObject;
-		animate = sprite.GetComponent<Animator>();
 	}
 
 	void Update () 
@@ -30,49 +28,22 @@ public class PlayerController : MonoBehaviour {
 			target.z = 0;
 			transform.position = Vector3.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
 		}
-		bool moved = false;
 		if (Input.GetKey (KeyCode.RightArrow)) {
 				transform.Rotate (0f, 0f, -2.5f);
 				
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 				transform.Rotate (0f, 0f, 2.5f);
-
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.position += transform.up * Time.deltaTime * movementSpeed;
-			moved = true;
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.position -= transform.up * Time.deltaTime * movementSpeed;
-			moved = true;
-
 		}
-		if (moved)
-			animate.Play ("walking");
-		else
-			animate.Play ("idle");
 
 		Vector3 pos = transform.position;
 		pos.z = -1;
 		spotlight.transform.position = pos;
-	}
-
-	void OnTriggerEnter2D( Collider2D col )
-	{
-		if (col.gameObject.tag == "agent") 
-		{
-			adjacentAgents.Add(col.gameObject);
-		}
-
-	}
-
-	void OnTriggerExit2D( Collider2D col )
-	{
-		if (col.gameObject.tag == "agent") 
-		{
-			adjacentAgents.Remove(col.gameObject);
-
-		}
 	}
 }
